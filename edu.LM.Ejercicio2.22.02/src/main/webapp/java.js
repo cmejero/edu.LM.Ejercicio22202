33060d03c9e559b058c2895b6fe188c8fca2f460
CARLOSMR\Carlo <csi23-cmejero@colegioaltair.net>
2024-03-06 21:34:32 +0100

    no message

---
+++ edu.LM.Ejercicio2.22.02/src/main/webapp/java.js
@@ -0,0 +1,14 @@
+fecha={
+dia: 6,
+mes: 3,
+anyo: 2024}
+
+calendario={
+dia: 6,
+mes: "Marzo",
+anyo: 2024}
+
+if ( fecha.mes==3 && calendario.mes=="Marzo" )
+	alert ("Es hoy");
+else
+alert ("Aún falta");
